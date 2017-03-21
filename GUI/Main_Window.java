2d85b3bb7ea6763f7bcfd39b6656c696bb2d961e
Rakesh Mane <rakeshmane12345@gmail.com>
2017-03-21 22:22:11 +0530

    Update Main_Window.java

--- GUI/Main_Window.java
+++ GUI/Main_Window.java
@@ -1,7 +1,8 @@
 
 package focus;
 
-import javafx.application.Application;import javafx.collections.ObservableList;
+import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
